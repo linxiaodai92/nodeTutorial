00311d8c43a6eda99289a277e96106e277cee253
linxiaodai92 <linxiaodai92@gmail.com>
2015-04-01 18:10:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.use(express.static('./public'));
 app.get('/', function(req, res) {
 	res.sendfile('./public/index.html');
 });
-
+app.get('/storytime', function(reg,res){
+res.send("<p>"+ req.query.name+ "nice to meet you! " + req.query.verb+ "this has been an awesome story of "+ req.query.adjective+
+", you are totally welcome</p");	
+});
 app.listen(app.get('port'), function() {
 	console.log('Fuck it ship it.');
 });
